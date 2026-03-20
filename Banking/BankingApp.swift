f65e73989a95ca44dfc40407a923a5990880201b
Rodrigo Cavalcanti <rodrigocav94@gmail.com>
2026-03-20 13:06:54 -0300

    Initial Commit

---
+++ Banking/BankingApp.swift
@@ -0,0 +1,17 @@
+//
+//  BankingApp.swift
+//  Banking
+//
+//  Created by Rodrigo Cavalcanti on 3/20/26.
+//
+
+import SwiftUI
+
+@main
+struct BankingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Banking/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Banking
+//
+//  Created by Rodrigo Cavalcanti on 3/20/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
